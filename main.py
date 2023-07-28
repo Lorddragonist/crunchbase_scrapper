c04d9d93260af1eaa4b4660e07f39b93f2ce4648
Andres Gonzalez <andrewgonzlez@gmail.com>
2023-07-28 09:15:35 -0500

    usando pyppeteer

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
-import requests
-from bs4 import BeautifulSoup as bs
-import random
-import time
-import pandas as pd
-import numpy as np
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import undetected_chromedriver as uc
-import time
+# import requests
+# from bs4 import BeautifulSoup as bs
+# import random
+# import time
+# import pandas as pd
+# import numpy as np
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# import undetected_chromedriver as uc
+# import time
 
-def run():
-    browser = uc.Chrome()    
-    url = 'https://news.crunchbase.com/public/makeup-venture-unicorn-ipo-oddity/'
-    browser.get(url)
-    browser.implicitly_wait(10)
+# def run():
+#     browser = uc.Chrome()    
+#     url = 'https://news.crunchbase.com/public/makeup-venture-unicorn-ipo-oddity/'
+#     browser.get(url)
+#     browser.implicitly_wait(10)
 
-    html = browser.page_source
-    soup = bs(html, 'lxml')
+#     html = browser.page_source
+#     soup = bs(html, 'lxml')
 
-    title = soup.find('header',{'class':'entry-header'}).find('h1',{'class':'entry-title h1'}).text
+#     title = soup.find('header',{'class':'entry-header'}).find('h1',{'class':'entry-title h1'}).text
 
-    author = soup.find('span',{'class':'vcard author'}).find('a').text
+#     author = soup.find('span',{'class':'vcard author'}).find('a').text
 
-    print('My title is:',title)
-    print('My author is:',author)
+#     print('My title is:',title)
+#     print('My author is:',author)
 
 
-if __name__ == "__main__":
-    run()
+# if __name__ == "__main__":
+#     run()
+
+import asyncio
+from pyppeteer import launch
+
+async def main():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto('https://www.example.com')
+    await page.screenshot({'path': 'example.png'})
+    await browser.close()
+
+asyncio.get_event_loop().run_until_complete(main())
